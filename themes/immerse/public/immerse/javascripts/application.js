9f03981f1824fd3ec88e855ac7fc0f2e8da770bc
Chad Ostrowski <chad.ostrowski@gmail.com>
2012-03-03 14:12:49 -0500

    columns, take 1 (no adjustment on resize; no button to scroll)

---
+++ themes/immerse/public/immerse/javascripts/application.js
@@ -0,0 +1,47 @@
+var colCountProp = '';
+var cols;
+
+jQuery.fn.extend({
+  h: function(){ return this.outerHeight(true); },
+  w: function(){ return this.outerWidth(true); }
+});
+
+function setColCountProp() {
+  cols = $('#columns');
+  if(cols.css('column-count') == 1)
+    colCountProp = 'column-count';
+  else if(cols.css('-webkit-column-count') == 1)
+    colCountProp = '-webkit-column-count';
+  else if(cols.css('-moz-column-count') == 1)
+    colCountProp = '-moz-column-count';
+  else if(cols.css('-o-column-count') == 1)
+    colCountProp = '-o-column-count';
+}
+
+function height() {
+  return $('#columns').h();
+}
+function idealHeight() {
+  return ($(window).h() - $('nav#main').h()) * .8;
+}
+
+function adjustHeight() {
+  while(height() > idealHeight()) {
+    addColumn();
+  }
+}
+
+function addColumn() {
+  var viewingWidth = $('#column-wrap').w();
+  var colsPermittedInView = Math.floor(viewingWidth / 300);
+  var nextColCount = +cols.css(colCountProp) + 1;
+  if(colsPermittedInView < nextColCount) {
+    cols.css('width', nextColCount / colsPermittedInView * viewingWidth);
+  }
+  cols.css(colCountProp, '' + (nextColCount));
+}
+
+$(function(){
+  setColCountProp();
+  adjustHeight();
+});
