9ff71dac112d3df74b4e0ad689fd8635a29f5c55
Chad Ostrowski <hi@chadoh.com>
2013-09-15 22:34:03 -0400

    Blog post for tomorrow and new scrolling behavior

--- themes/immerse/public/immerse/javascripts/application.js
+++ themes/immerse/public/immerse/javascripts/application.js
@@ -41,11 +41,57 @@ function addColumn() {
   cols.css(colCountProp, '' + (nextColCount));
 }
 
+function scrollToColumn(index) {
+  if (isNaN(index))
+    index = 0;
+  var totalWidth = cols.w();
+  var colCount = cols.css(colCountProp);
+  var colWidth = totalWidth / colCount;
+  var desiredOffset = colWidth * index;
+  var currentOffset =  cols.css('left');
+
+  cols.animate({ 'left': -desiredOffset });
+}
+
+addScrollButtons = function() {
+  var wrap = $('#column-wrap');
+  wrap.css({'overflow': 'hidden'});
+  wrap.append($('<a title="Scroll left"  href="#-1" class="icon-chevron-left"  style="float: left;  width: 50%;">'));
+  wrap.append($('<a title="Scroll right" href="#1"  class="icon-chevron-right" style="float: right; width: 50%; text-align: right;">'));
+}
+
+hideRightChevronMaybe = function(currentColumn){
+  var viewingWidth = $('#column-wrap').w();
+  var colsPermittedInView = Math.floor(viewingWidth / 300);
+  if (currentColumn + colsPermittedInView == cols.css(colCountProp))
+    $('a.icon-chevron-right').hide()
+  else
+    $('a.icon-chevron-right').show()
+}
+
+updateScrollButtons = function(currentColumn){
+  if (isNaN(currentColumn))
+    currentColumn = 0;
+  $('a.icon-chevron-left').attr('href', '#' + (currentColumn - 1));
+  $('a.icon-chevron-right').attr('href', '#' + (currentColumn + 1));
+  hideRightChevronMaybe(currentColumn);
+}
+
 $(function(){
+  hljs.initHighlightingOnLoad();
+
   if(! $('body').hasClass('no-columns') && ($(window).w() > 625)){
     setColCountProp();
     adjustHeight();
+    addScrollButtons();
   }
+
+  $(window).hashchange(function(){
+    var desiredCol = +window.location.hash.replace('#', '');
+    scrollToColumn(desiredCol);
+    updateScrollButtons(desiredCol);
+  });
+  $(window).hashchange();
 
   /* fire a `resizeEnd` function when window resizing pauses for >=500ms
    * from http://stackoverflow.com/a/2996465/249801 */
@@ -66,3 +112,17 @@ $(function(){
   });
 
 });
+
+$(document).keypress(function(e){
+  if (e.keyCode == 104) { // left arrow
+    var newCol = $('a.icon-chevron-left:visible').attr('href');
+    if (newCol) window.location = newCol;
+    console.log(newCol);
+  }
+  else if (e.keyCode == 108) { // right arrow
+    $('a.icon-chevron-right:visible').attr('href');
+    var newCol = $('a.icon-chevron-right:visible').attr('href');
+    if (newCol) window.location = newCol;
+  }
+});
+
